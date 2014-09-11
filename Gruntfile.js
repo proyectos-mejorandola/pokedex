a03b3cc3a9e610f00c14b0cad283fdf3edda971b
Julian Duque <julianduquej@gmail.com>
2014-09-11 01:22:52 -0500

    Add base project and livereload support

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'public/'
+        }
+      }
+    },
+    watch: {
+      project: {
+        files: ['public/**/*.js', 'public/**/*.html', 'public/**/*.json'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+
+};
