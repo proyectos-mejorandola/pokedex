1281fd723250d4027cc545cff6673fdc7643fd94
Julian Duque <julianduquej@gmail.com>
2014-09-11 23:31:30 -0500

    Add stats and evolution

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,17 @@
       type: [ "Grass", "Poison" ],
       height: "2′4″ (0.71m)",
       weight: "15.2 lbs (6.9 kg)",
-      abilities: [ "Overgrow", "Chlorophyll"]
+      abilities: [ "Overgrow", "Chlorophyll" ],
+      stats: {
+        hp: 45,
+        attack: 49,
+        defense: 49,
+        "sp.atk": 65,
+        "sp.def": 65,
+        speed: 45,
+        total: 318
+      },
+      evolution: [ "Bulbasaur", "Ivysaur", "Venusaur" ]
     };
   });
 
