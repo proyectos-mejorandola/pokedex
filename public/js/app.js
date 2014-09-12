1d03946d1b5dbbf97bd42d6bf2e3f43786d7cbfc
Julian Duque <julianduquej@gmail.com>
2014-09-12 00:22:53 -0500

    Create imageify filter

--- public/js/app.js
+++ public/js/app.js
@@ -33,4 +33,11 @@
 
   });
 
+  app.filter('imageify', function () {
+    return function (input) {
+      var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
+      return url;
+    };
+  });
+
 })();
