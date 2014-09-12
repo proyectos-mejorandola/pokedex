1c39cf2d5cf1793301835bdca701431a25bc9de0
Julian Duque <julianduquej@gmail.com>
2014-09-12 16:19:28 -0500

    Create comments controller and add show/hide functionality

--- public/js/app.js
+++ public/js/app.js
@@ -33,6 +33,16 @@
 
   });
 
+  app.controller('CommentsController', function () {
+    this.comments = [];
+    this.show = false;
+
+    this.toggle = function () {
+      this.show = !this.show;
+    };
+
+  });
+
   app.filter('imageify', function () {
     return function (input) {
       var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
