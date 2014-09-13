4d438bf182c4f7418e113b3a9345a1ad5da7b510
Julian Duque <julianduquej@gmail.com>
2014-09-13 00:31:03 -0500

    Add ngRoute and basic route to load the pokemon

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 (function () {
 
   var app = angular.module('pokedex', [
+    'ngRoute',
     'pokedex.controllers',
     'pokedex.directives',
     'pokedex.filters'
   ]);
+
+  app.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/pokemon.html',
+        controller: 'PokemonController',
+        controllerAs: 'pkmCtrl'
+      });
+
+  }]);
 
 })();
