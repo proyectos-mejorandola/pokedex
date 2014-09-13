67598d236550c413c5d2ace442b91fcf20399232
Julian Duque <julianduquej@gmail.com>
2014-09-12 23:27:57 -0500

    Create directives for other pokemon attributes

--- public/js/app.js
+++ public/js/app.js
@@ -63,11 +63,40 @@
     };
   });
 
+  app.directive('pokemonName', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-name.html'
+    };
+  });
+
+  app.directive('pokemonImage', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-image.html'
+    };
+  });
+
   app.directive('pokemonData', function () {
     return {
       restrict: 'E',
       templateUrl: 'partials/pokemon-data.html'
-    }
+    };
   });
+
+  app.directive('pokemonStats', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/pokemon-stats.html'
+    };
+  });
+
+  app.directive('pokemonEvolution', function () {
+    return {
+      retrict: 'E',
+      templateUrl: 'partials/pokemon-evolution.html'
+    };
+  });
+
 
 })();
