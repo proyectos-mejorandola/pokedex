417855e31044ef6c812b4b8ddc37e970a5145bdd
Julian Duque <julianduquej@gmail.com>
2014-09-14 03:04:30 -0500

    Implement save/get comments from service

--- public/js/directives.js
+++ public/js/directives.js
@@ -43,33 +43,45 @@
       };
     })
 
-    .directive('pokemonComments', function () {
+    .directive('pokemonComments', ['pokemonService', function (pokemonService) {
       return {
         restrict: 'E',
         templateUrl: 'partials/pokemon-comments.html',
-        controller: function () {
-          this.comments = [];
-          this.comment = {};
-          this.show = false;
+        scope: {
+          name: '@name'
+        },
+        link: function (scope, element, attributes) {
+          attributes.$observe('name', function (value) {
+            if (value) {
+              scope.name = value;
+              scope.comments = pokemonService.getComments(value);
+            }
+          });
+        },
+        controller: function ($scope) {
+          $scope.comments = pokemonService.getComments($scope.name);
+          $scope.comment = {};
+          $scope.show = false;
 
-          this.toggle = function () {
-            this.show = !this.show;
+          $scope.toggle = function () {
+            $scope.show = !$scope.show;
           };
 
-          this.anonymousChanged = function () {
-            if (this.comment.anonymous) {
-              this.comment.email = "";
+          $scope.anonymousChanged = function () {
+            if ($scope.comment.anonymous) {
+              $scope.comment.email = "";
             }
           };
 
-          this.addComment = function () {
-            this.comment.date = Date.now();
-            this.comments.push(this.comment);
-            this.comment = {};
+          $scope.addComment = function () {
+            $scope.comment.date = Date.now();
+            pokemonService.saveComment($scope.name, $scope.comment);
+            $scope.comments = pokemonService.getComments($scope.name);
+            $scope.comment = {};
           };
-        },
-        controllerAs: 'cmtsCtrl'
+
+        }
       };
-    });
+    }]);
 
 })();
