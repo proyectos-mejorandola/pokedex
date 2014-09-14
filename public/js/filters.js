170f6934a9f744cc5c66a072b07abb0e01cebe51
Julian Duque <julianduquej@gmail.com>
2014-09-13 21:53:47 -0500

    Add filter to normalize images

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,11 +1,21 @@
 (function () {
 
   angular.module('pokedex.filters', [])
-    .filter('imageify', function () {
+    .filter('normalize', function () {
       return function (input) {
-        var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
+          input = input
+                  .replace('♀', 'f')
+                  .replace('♂', 'm')
+                  .replace(/\W+/g, "");
+          return input.toLowerCase();
+      };
+    })
+
+    .filter('imageify', ['$filter', function ($filter) {
+      return function (input) {
+        var url = "img/pokemons/" + $filter('normalize')(input) + ".jpg";
         return url;
       };
-    });
+    }]);
 
 })();
