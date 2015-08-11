1525dd8c292d82f08266b78ac3052b1d6fa14513
Alberto D.Sosa <albertodsosa@gmail.com>
2015-08-11 22:48:27 +0100

    My todo_3

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,8 @@
 (function (_) {
 
   angular.module('pokedex.controllers', [])
-    .controller('PokedexController', ['$scope', '$routeParams', 'pokemonService', function ($scope, $routeParams, pokemonService) {
+    .controller('PokedexController', ['$rootScope', '$scope', '$routeParams', 'pokemonService', function ($rootScope, $scope, $routeParams, pokemonService) {
+      $rootScope.title = '';
       var type = $routeParams.type;
 
       if (type) {
@@ -27,13 +28,14 @@
 
     }])
 
-    .controller('PokemonController', ['$scope', '$routeParams', 'pokemonService', function ($scope, $routeParams, pokemonService) {
+    .controller('PokemonController', ['$rootScope', '$scope', '$routeParams', 'pokemonService', function ($rootScope, $scope, $routeParams, pokemonService) {
       var name = $routeParams.name;
       $scope.error;
 
 
       pokemonService.byName(name)
       .then(function (data) {
+        $rootScope.title = data.name;
         $scope.pokemon = data;
       }, function (reason) {
         $scope.error = reason;
